de404f22f8c2d97c31b5d7c9b9d31e9febe98f33
Lane <lolson@ualberta.ca>
2012-04-27 15:01:39 -0600

    Refactoring, adding destroy/create methods, add option for event type

--- dropdown.js
+++ dropdown.js
@@ -24,6 +24,12 @@ Description:	Displays a list of links as a dropdown menu
 		// Access to jQuery and DOM versions of element
 		base.$el = $(el);
 		base.el = el;
+		
+		// Link to show/hide links
+		var linkEl, subNav;
+		
+		// to keep track of plugin state
+		var isCreated = false;
 
 		// Add a reverse reference to the DOM object
 		base.$el.data( "responsive.dropdown" , base );
@@ -31,58 +37,113 @@ Description:	Displays a list of links as a dropdown menu
 		base.init = function () {
 		
 			base.options = $.extend({}, $.responsive.dropdown.defaultOptions, options);
+			
+			base.$el.bind("createdropdown", function() {
+				base.create();
+			});
+			
+			base.$el.bind("destroydropdown", function() {
+				base.destroy();
+			});
 
-			base.$el.find('ul:first').wrap('<ul><li></li></ul>');
-			base.$el.find('ul li:first').prepend('<a href="#">'+base.options.linkText+'</a>');
-			
-			// Link to show/hide links
-			var linkEl = base.$el.find("ul li a:first");
-			var subNav = linkEl.siblings('ul');
-			
-			if(!base.options.showLinksByDefault)
-				subNav.hide();
-			
-			// Add the arrow to the link
-			linkEl.append('<strong>'+ base.options.htmlArrowMore +'</strong>');
+			base.create();
+		};
 		
-			// Click event for show/hide navigation link
-			linkEl.click(function() {
+		base.create = function() {
+			if(!isCreated)
+			{
+				// Wrap existing list in another list
+				base.$el.find('ul:first').wrap('<ul><li></li></ul>');
 				
-				if(subNav.is(":hidden")) {
+				// Add the show/hide link
+				base.$el.find('ul li:first').prepend(
+					'<a class="toggle-nav" href="#">'+base.options.linkText+'</a>'
+				);
 				
-					subNav.css('width',
-						base.$el.find('ul:first').outerWidth()-(parseInt(subNav.css('border-left-width'),10)+parseInt(subNav.css('border-right-width'),10))
-					).slideDown('fast').show();
-					
-					$('strong', linkEl).html(base.options.htmlArrowLess);
-					
-					subNav.parent().hover(function() { }, function() {  
-						if(!linkEl.is(":hidden")) {
-							// When the mouse hovers out of the subnav, move it back up 
-							subNav.slideUp('fast', function() { $(this).css("width", "auto"); }); 
-							$('strong', linkEl).html(base.options.htmlArrowMore);
-						}
+				// Assign the subnav
+				linkEl = $('.toggle-nav', base.el);
+				subNav = linkEl.siblings('ul');
+				
+				// Add the arrow to the link
+				linkEl.append('<strong>'+ base.options.htmlArrowMore +'</strong>');
+				
+				// Hide dropdown if not visible by default
+				if(!base.options.showLinksByDefault)
+					subNav.hide();
+				
+				if(base.options.bindOn == 'hover')
+				{
+					linkEl.parent().bind('mouseenter', function(event) {
+						base.showDropdown();
+						return false;
 					});
 					
-				} else {
-				
-					subNav.slideUp('fast', function() { $(this).css("width", "auto"); });
-					$('strong', linkEl).html(base.options.htmlArrowMore);
-					
+					linkEl.parent().bind('mouseleave', function() {
+						base.hideDropdown();
+						return false;
+					});
 				}
+				if(base.options.bindOn == 'click')
+				{
+					// hide dropdown on click outside of menu
+					$('html').click(function() {
+						base.hideDropdown();
+					});
+					
+					linkEl.bind('click', function(event) {
+						// do not propagate event
+						event.stopPropagation();
+						
+						if(subNav.is(':hidden')) {
+							base.showDropdown();
+						} else {
+							base.hideDropdown();
+						}
+						return false;
+					});
+				}
+				isCreated = true;
+			}
+		};
+		
+		base.showDropdown = function() {
+			subNav.slideDown('fast');
+			$('strong', linkEl).html(base.options.htmlArrowLess);
+		};
+		
+		base.hideDropdown = function() {
+			subNav.slideUp('fast');
+			$('strong', linkEl).html(base.options.htmlArrowMore);
+		};
+		
+		base.destroy = function() {
+			
+			if(isCreated) {
+				// remove from wrapper
+				base.$el.html(subNav);
 				
-				return false;
-			});
+				// remove any inline styling
+				$('ul', base.el).removeAttr('style');
+				
+				// unbind events
+				linkEl.parent().unbind('mouseenter mouseleave');
+				linkEl.unbind('click');
+	
+				linkEl = null;
+				subNav = null;
+				isCreated = false;
+			}
 		};
 		
 		base.init();
 	};
 	
 	$.responsive.dropdown.defaultOptions = {
-		linkText: 'Navigation',
+		linkText: 'Navigation', // Wording for collapsed menu
 		htmlArrowMore: '&#x25BC;', // html to represent more arrow
 		htmlArrowLess: '&#x25B2;', // html to represent less arrow
-		showLinksByDefault: false
+		bindOn: 'hover',  // what event to trigger the subnav ('click' or 'hover')
+		showLinksByDefault: false // display navigation links by default
 	};
 	
 	$.fn.dropdown = function( options ) {
